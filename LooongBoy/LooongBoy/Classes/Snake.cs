a23dac75af40b8dffbabac72dac9e9a461e35992
NyoomNyoom <jacksonnorth1275@gmail.com>
2023-07-15 14:56:31 +1200

    Started working on get and set methods for the snake class

---
+++ LooongBoy/LooongBoy/Classes/Snake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooongBoy.Classes
+{
+    public class Snake
+    {
+        private bool IsDead = false;
+        private Point Direction = new Point();
+        private List<Point> Body = new List<Point>();
+
+        //Writing get and set methods for the private variables.
+        //IsDead get & set method.
+        public bool GetIsDead()
+        {
+            return IsDead;
+        }
+
+        public void SetIsDead(bool Input)
+        {
+            IsDead = Input;
+        }
+
+
+    }
+}
