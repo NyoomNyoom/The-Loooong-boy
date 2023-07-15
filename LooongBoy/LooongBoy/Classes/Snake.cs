d1d2e445592d50a4ca1e5c1b9a67be7ce5ebfef0
NyoomNyoom <jacksonnorth1275@gmail.com>
2023-07-15 15:11:26 +1200

    Did some more work on the snake class.
    Added get and set methods for body and direction. then did some work on the default constructor.

--- LooongBoy/LooongBoy/Classes/Snake.cs
+++ LooongBoy/LooongBoy/Classes/Snake.cs
@@ -9,8 +9,8 @@ namespace LooongBoy.Classes
     public class Snake
     {
         private bool IsDead = false;
-        private Point Direction = new Point();
-        private List<Point> Body = new List<Point>();
+        private Point Direction = new();
+        private List<Point> Body = new();
 
         //Writing get and set methods for the private variables.
         //IsDead get & set method.
@@ -24,6 +24,52 @@ namespace LooongBoy.Classes
             IsDead = Input;
         }
 
+        //Direction get & set.
+        public Point GetDirection()
+        {
+            return Direction;
+        }
+
+        public void SetDirection(Point Input)
+        {
+            Direction = Input;
+        }
+
+        //Body get and set methods.
+        public List<Point> GetBody()
+        {
+            return Body;
+        }
+
+        public void SetBody(List<Point> input)
+        {
+            Body = input;
+        }
+
+        //Default constructor for the class
+        public Snake()
+        {
+            AddSegment();
+            SetDirection(new Point(1,0)); //Initial direction of facing right.
+        }
+
+        //Code to add a segment to the body
+        public void AddSegment()
+        {
+            //Checking if there is a body to add to, or if it needs to be initilized.
+            if(Body.Count > 0)
+            {
+                Body.Add(Body[Body.Count - 1]);
+            }else if(Body.Count == 0) 
+            {
+                Body.Add(new Point(0, 0));
+            }
+            else
+            {
+                return;
+            }
+            
+        }
 
     }
 }
